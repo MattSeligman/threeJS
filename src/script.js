19e26fe53b24e8856c06ac820fa3e88b81b7c7ab
MattSeligman <matt@modelitup.com>
2022-07-22 22:04:53 -0700

    Add Wireframe test

--- src/script.js
+++ src/script.js
@@ -34,7 +34,7 @@ group.position.z = Math.PI * 0;
 
 const cube1 = new THREE.Mesh(
 	new THREE.BoxGeometry(1, 1, 1),
-	new THREE.MeshBasicMaterial({ color: 0xff0000 })
+	new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 );
 group.add(cube1);
 
