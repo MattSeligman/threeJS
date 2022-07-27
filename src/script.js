40dcf01541224406a5827cc18f112074232c5f28
MattSeligman <matt@modelitup.com>
2022-07-27 15:30:16 -0700

    Add Light's with Light Helpers

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
 import gsap from 'gsap';
 import GUI from 'lil-gui';
 
@@ -248,13 +249,74 @@ material.transparent = true;
 /**
  * Object Lighting
  */
-const point = new THREE.PointLight(0xffffff, 0.5);
-const light = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(light, point);
 
-point.position.x = 2;
-point.position.y = 0;
-point.position.z = 4;
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(1, 0.25, 0);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff000, 0x0000ff, 0.3);
+scene.add(hemisphereLight);
+hemisphereLight.position.set(1, 0.25, 0);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5);
+scene.add(pointLight);
+pointLight.position.set(1, -0.5, 1);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-0.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(
+	0x78ff00,
+	0.5,
+	10,
+	Math.PI * 0.1,
+	0.25,
+	1
+);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+spotLight.target.position.x = 7;
+scene.add(spotLight.target);
+
+// Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+	hemisphereLight,
+	0.1
+);
+scene.add(hemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+	directionalLight,
+	0.1
+);
+scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
+
+window.requestAnimationFrame(() => {
+	spotLightHelper.update();
+});
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+scene.add(rectAreaLightHelper);
+
+// const point = new THREE.PointLight(0xffffff, 0.5);
+// const light = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(light, point);
+
+// point.position.x = 2;
+// point.position.y = 0;
+// point.position.z = 4;
 
 /**
  * Objects
@@ -292,6 +354,14 @@ torus.geometry.setAttribute(
 
 torus.position.y = 3.5;
 torus.position.x = -2.5;
+
+const groundPlane = new THREE.Mesh(
+	new THREE.PlaneGeometry(100, 100, 100, 100),
+	material
+);
+groundPlane.position.set(0, -1, 0);
+groundPlane.rotation.set(-1.5, 0, 0);
+scene.add(groundPlane);
 
 const group = new THREE.Group();
 let hideObjects = {
