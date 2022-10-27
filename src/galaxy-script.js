8216d4bf0475367676489bbe8e04385b578918bf
MattSeligman <matt@modelitup.com>
2022-10-27 15:54:46 -0700

    Setup point cloud positions

---
+++ src/galaxy-script.js
@@ -0,0 +1,131 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Galaxy
+ */
+
+const parameters = {
+	count: 1000,
+	size: 0.01,
+};
+
+const generateGalaxy = () => {
+	/**
+	 * Geometry
+	 */
+	const geometry = new THREE.BufferGeometry();
+
+	const positions = new Float32Array(parameters.count * 3);
+
+	for (let i = 0; i < parameters.count; i++) {
+		const i3 = i * 3;
+
+		positions[i3] = (Math.random() - 0.5) * 3; // x
+		positions[i3 + 1] = (Math.random() - 0.5) * 3; // y
+		positions[i3 + 2] = (Math.random() - 0.5) * 3; // z
+	}
+
+	geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+	/**
+	 * Material
+	 */
+
+	const material = new THREE.PointsMaterial({
+		size: parameters.size,
+		sizeAttenuation: true,
+		depthWrite: false,
+		blending: THREE.AdditiveBlending,
+	});
+
+	/**
+	 * Points
+	 */
+	const points = new THREE.Points(geometry, material);
+	scene.add(points);
+
+	console.log('generateGalaxy');
+	console.log(positions);
+};
+
+generateGalaxy();
+
+/**
+ * Sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime();
+
+	// Update controls
+	controls.update();
+
+	// Render
+	renderer.render(scene, camera);
+
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
+};
+
+tick();
